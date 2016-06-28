73efd7b7379c0e06c725400ab2452be08dc9ad7a
Louis Opter <kalessin@kalessin.fr>
2016-06-27 21:21:53 -0700

    Modify the ergodox buildscript for my configuration

--- Keyboards/ergodox.bash
+++ Keyboards/ergodox.bash
@@ -23,7 +23,7 @@ BaseMap="defaultMap leftHand slave1 rightHand"
 # This is the default layer of the keyboard
 # NOTE: To combine kll files into a single layout, separate them by spaces
 # e.g.  DefaultMap="mylayout mylayoutmod"
-DefaultMap="mdergo1Overlay lcdFuncMap"
+DefaultMap="louis_lmain lcdFuncMap"
 
 # This is where you set the additional layers
 # NOTE: Indexing starts at 1
@@ -31,8 +31,8 @@ DefaultMap="mdergo1Overlay lcdFuncMap"
 # e.g.  PartialMaps[1]="layer1 layer1mod"
 #       PartialMaps[2]="layer2"
 #       PartialMaps[3]="layer3"
-PartialMaps[1]="iced_func"
-PartialMaps[2]="iced_numpad"
+PartialMaps[1]="lcdFuncMap louis_l1"
+PartialMaps[7]="lcdFuncMap louis_l7"
 
 
 
